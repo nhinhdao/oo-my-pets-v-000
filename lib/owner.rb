bffb3b5814789fb73308d1122d55af5b276254d8
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-06-29 23:43:32 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,17 @@
 class Owner
   # code goes here
+  attr_accessor :species, :pets
+
+  def initialize(species)
+      @species = species
+  end
+  
+  def all
+  end
+
+  def self.reset_all
+
+  end
+
+  
 end
