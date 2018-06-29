9834fb9a1933a137d7a626c94ec2faedc020d89d
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-06-29 23:50:15 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -14,6 +14,31 @@ class Owner
     def say_species
     end
 
+    def buy_fish(name)
+    end
+
+    def buy_cat(name)
+    end
+
+    def buy_dog(name)
+    end
+
+    def walk_dogs
+    end
+
+    def play_with_cats
+    end
+
+    def feed_fish
+    end
+
+    def sell_pets
+    end
+
+    def list_pets
+        puts "I have 2 fish, 3 dog(s), and 1 cat(s)."
+    end
+
     def all
     end
 
