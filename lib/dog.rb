64e2daf0ef8e51a274771c72e9667eaca27b8756
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-06-29 23:59:35 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,14 @@
 class Dog
-  # code goes here
+    # code goes here
+    attr_reader :name
+    attr_accessor :mood
+    
+    def initialize(name)
+        @name = "Daisy"
+        @mood = "nervous"
+    end
+    
+    def mood=(mood)
+        @mood = mood
+    end
 end
