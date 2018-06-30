d794ec1d7e5f26b833f6eb8f0bfb98cdf16dcb2c
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-06-30 00:00:31 +0000

    automatically backed up by learn

--- lib/fish.rb
+++ lib/fish.rb
@@ -1,3 +1,14 @@
 class Fish
-  # code goes here
+    attr_reader :name
+    attr_accessor :mood
+
+    def initialize(name)
+        @name = "Nemo"
+        @mood = "nervous"
+    end
+
+    def mood=(mood)
+        @mood = mood
+    end
+
 end
